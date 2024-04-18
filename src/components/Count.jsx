fd0c199e6542e2d97cd1e74f2c2d7b529a960987
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-18 11:05:22 +0200

    Added a loggedCount value to be able to display it in the DOM.

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -2,12 +2,13 @@ import { logCount } from "../utils/logCount";
 import { useCountStore } from "../stores/useCountStore";
 
 export const Count = () => {
-  const { count, increment, decrement, reset } = useCountStore();
+  const { count, loggedCount, increment, decrement, reset } = useCountStore();
 
   return (
     <div>
           <h2>Count</h2>
           <p>Count: {count}</p>
+          <p>Before reset Count: {loggedCount}</p>
           <button onClick={increment}>Increment</button>
           <button onClick={decrement}>Decrement</button>
           <button onClick={reset}>Reset</button>
--- src/stores/useCountStore.jsx
+++ src/stores/useCountStore.jsx
@@ -2,11 +2,13 @@ import { create } from "zustand"
 
 export const useCountStore = create((set, get) => ({
     count: 0,
+    loggedCount: 0,
     increment: () => set(state => ({ count: state.count + 1 })),
     decrement: () => set(state => ({ count: state.count - 1 })),
     // reset: () => set({ count: 0 })
     reset: () => {
         console.log("Current count before reset: ", get().count);
-        set({count: 0})
+        set({ loggedCount: get().count });
+        set({ count: 0 });
     }
 }))
